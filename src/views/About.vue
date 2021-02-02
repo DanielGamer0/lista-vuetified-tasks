7f8a6f0ae12d57a8ef6d10020cd1bb08ae79afad
DanielGamer0 <Je.Murga@alumnos.duoc.cl>
2021-02-01 23:27:40 -0300

    cse actualiza el 👨‍💻

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <p>CRUD básico de lista de tareas y otras demostraciones simples realizadas en Vuetify </p>
+    <p>CRUD básico de lista de tareas y otras demostraciones simples realizadas en Vuetify.</p>
     <p>✍️ por ダニエルゲーマー0 👨‍💻</p>
   </div>
 </template>
